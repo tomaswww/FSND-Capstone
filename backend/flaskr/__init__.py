31f9aaf9114daecda3793717c538316223c2cf30
tomaswww <twingord@gmail.com>
2019-11-12 00:11:06 -0300

    Added info to the readme file

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, abort, jsonify, render_template, \
+    Response, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+from models import *
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  ''' @app.route('/endpoint', methods=['GET'])
+      def function():
+      ...
+      return jsonify{(...)}
+  '''
+
+  return app
+
+
+APP = create_app()
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=8080, debug=True)
