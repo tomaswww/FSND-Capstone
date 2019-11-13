ce1fbf85ada2ab0b0f494bf41c046279042e2c35
tomaswww <twingord@gmail.com>
2019-11-13 16:07:11 -0300

    Added error handlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -175,6 +175,48 @@ def create_app(test_config=None):
         one_movie["release_date"] = new_movie.release_date
         return jsonify({"success": True, "Movie": one_movie})
 
+    # @TODO: Create error handlers for all expected errors \
+    #  including 404 and 422. --> DONE
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity"
+        }), 422
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method not allowed"
+        }), 405
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request error"
+        }), 400
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal server error"
+        }), 500
+
     return app
 
 
