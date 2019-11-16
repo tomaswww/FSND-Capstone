4527f4b87f3de587915042ce1c12ee1a6e94a4fa
tomaswww <twingord@gmail.com>
2019-11-16 02:48:00 -0300

    Added README info

---
+++ backend/README.md
@@ -0,0 +1,61 @@
+## Getting Started
+
+### Installing Dependencies
+
+
+#### Python 3.7
+
+Follow instructions to install the latest version of python for your platform in the [python docs](https://docs.python.org/3/using/unix.html#getting-and-installing-the-latest-version-of-python)
+
+#### Virtual Enviornment
+
+We recommend working within a virtual environment whenever using Python for projects. This keeps your dependencies for each project separate and organaized. Instructions for setting up a virual enviornment for your platform can be found in the [python docs](https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/)
+
+#### PIP Dependencies
+
+Once you have your virtual environment setup and running, install dependencies by naviging to the `/backend` directory and running:
+
+```bash
+pip install -r requirements.txt
+```
+
+This will install all of the required packages we selected within the `requirements.txt` file.
+
+##### Key Dependencies
+
+- [Flask](http://flask.pocoo.org/)  is a lightweight backend microservices framework. Flask is required to handle requests and responses.
+
+- [SQLAlchemy](https://www.sqlalchemy.org/) is the Python SQL toolkit and ORM we'll use handle the lightweight sqlite database. You'll primarily work in app.py and can reference models.py. 
+
+- [Flask-CORS](https://flask-cors.readthedocs.io/en/latest/#) is the extension we'll use to handle cross origin requests from our frontend server. 
+
+## Configuring the DB
+
+For this project you'll have to setup a local DB and then complete the info about it on the [`models.py`](models.py) file.
+
+## Example DB
+
+For an example DB you can create manually using the following [`Example.txt`](example.txt)
+
+## Running the server
+
+From within the `backend` directory first ensure you are working using your created virtual environment.
+
+To run the server, execute:
+
+```bash
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
+```
+
+Setting the `FLASK_ENV` variable to `development` will detect file changes and restart the server automatically.
+
+Setting the `FLASK_APP` variable to `flaskr` directs flask to use the `flaskr` directory and the `__init__.py` file to find the application. 
+
+## Avaible Endpoints
+
+GET /actors and /movies
+DELETE /actors/ and /movies/
+POST /actors and /movies and
+PATCH /actors/ and /movies/
--- frontend/casting/src/components/ViewArtists.js
+++ frontend/casting/src/components/ViewArtists.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from 'react';
 import {Component} from 'react';
+import axios from 'axios';
 import logo from './artist.png';
-
 import { Link } from "react-router-dom";
-import { useAuth0} from "../react-auth0-spa";
+import { useAuth0, Auth0Context,Auth0Provider}  from "../react-auth0-spa";
 import $ from "jquery";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 const ViewArtist = () => {
   const { loading, user } = useAuth0();
-
   
 
   if (loading || !user) {
@@ -17,28 +18,37 @@ const ViewArtist = () => {
       <div>Loading...</div>
     );
   }
-
-  return (
-    <Fragment>
-        <body className="App-body">
-            <img src={logo} alt="artist" />
-            <h4>Loged in as: {user.nickname}</h4>
-            <span>
-                <div class="card">
-                    <div class="card-header">
-                        All Artists:
+  else {
+      const response = axios.get("http://localhost:5000/actors");
+      console.log(response.data);
+                  
+      return (
+        <Fragment>
+            <script>
+              const hello = $.getJSON('https://capstone-auth.auth0.com/oauth/token');
+              console.log(hello);
+            </script>
+            <body className="App-body">
+                <img src={logo} alt="artist" />
+                <h4>Loged in as: {user.nickname}</h4>
+                <h4 id="tokenHere">Token:</h4>
+                <span>
+                    <div class="card">
+                        <div class="card-header">
+                            All Artists:
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Artist Array</h5>
+                            <p class="card-text" id="artistsData"></p>
+                            <a href="/Profile" class="btn btn-primary">Go Back</a>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Artist Array</h5>
-                        <p class="card-text" id="artistsData">ALL THE DATA</p>
-                        <a href="/Profile" class="btn btn-primary">Go Back</a>
-                    </div>
-                </div>
-            </span>
-      </body>
-    </Fragment>
-  );
-};
+                </span>
+          </body>
+        </Fragment>
+      );
+  };
+  };
 
 export default ViewArtist;
   
