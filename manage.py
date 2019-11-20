e8244f97dc5f2b83080b616a8123fce31280d701
tomaswww <twingord@gmail.com>
2019-11-20 11:44:50 -0300

    config heroku file

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from backend.flaskr.__init__.py import app
-from models import db
+from backend.flaskr.models.py import db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
