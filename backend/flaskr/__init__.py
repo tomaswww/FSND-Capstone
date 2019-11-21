012579d58aa65e8700851bbb44051636377ad17f
tomaswww <twingord@gmail.com>
2019-11-20 23:54:35 -0300

    Localhost

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -242,4 +242,4 @@ def create_app(test_config=None):
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=5432, debug=True)
+    APP.run(host='localhost', port=5432, debug=True)
